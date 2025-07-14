cd1633c8e628f31f32d5c15e44fe66b0bdcd48d8
Farias Maiquita <fmaiquita@gmail.com>
2025-07-14 16:32:25 +0200

    Add switzer font class to body element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import React from "react";
+import { switzer } from "@/app/fonts/fonts";
 
 export const metadata: Metadata = {
-  title: "T.Q.C",
-  description: "Framer Portfolio Template Clone",
+    title: "T.Q.C",
+    description: "Framer Portfolio Template Clone",
 };
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode }>) {
-  return (
-    <html lang="en">
-      <body className="">
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={switzer.className}>
+                {children}
+            </body>
+        </html>
+    );
 }
