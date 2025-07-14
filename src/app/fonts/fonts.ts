3dd444475619fcc2bd132fdd3c2b22c1a2c10173
Farias Maiquita <fmaiquita@gmail.com>
2025-07-14 16:31:15 +0200

    Add font.ts config

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+
+export const switzer = localFont({
+    src: [
+        {
+            path: "./Switzer-Regular.woff2",
+            weight: "400",
+            style: "normal"
+        }, {
+            path: "./Switzer-Semibold.woff2",
+            weight: "600",
+            style: "normal"
+        }
+    ]
+});
