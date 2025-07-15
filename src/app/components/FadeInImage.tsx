744362fa9d9a196c02547206fb797922ddc426c1
Farias Maiquita <fmaiquita@gmail.com>
2025-07-15 16:04:24 +0200

    Add FadeInImage.tsx component

---
+++ src/app/components/FadeInImage.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image, { ImageProps } from 'next/image';
+import { useState } from 'react';
+
+type FadeInImageProps = ImageProps & {
+    showPulse?: boolean;
+};
+
+function loadingPulseBg(shown: boolean) {
+    if (shown) {
+        return (
+            <div className="absolute inset-0 bg-white/10 animate-pulse"></div>
+        )
+    }
+
+    return null;
+}
+
+export default function FadeInImage({ className = '', showPulse = true, ...props }: FadeInImageProps) {
+    const [loaded, setLoaded] = useState(false);
+
+    return (
+        <>
+            {loadingPulseBg(showPulse)}
+            <Image fill={true} {...props} onLoad={() => setLoaded(true)} className={`object-cover transition-opacity duration-500 ${loaded ? 'opacity-100' : 'opacity-0'} ${className}`} />
+        </>
+    );
+}
