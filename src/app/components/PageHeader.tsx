f6c16109ebbfa114fec7927327c33569e623006f
Farias Maiquita <fmaiquita@gmail.com>
2025-07-15 16:03:53 +0200

    Fix media queries and z-index issue on PageHeader.tsx

--- src/app/components/PageHeader.tsx
+++ src/app/components/PageHeader.tsx
@@ -19,20 +19,20 @@ export default function PageHeader({initBgOpaque = true}: PageHeaderProps) {
     ];
 
     return (
-        <header className={`fixed inset-x-0 min-[810px]:px-[20px] px-[15px] min-[810px]:h-[58px] h-[49px] grid items-center min-[1200px]:grid-cols-3 grid-cols-2 gap-[20px] font-semibold min-[810px]:text-[18px] text-[14px] leading-none ${initBgOpaque ? "min-[1200px]:bg-white" : "min-[1200px]:text-white"} max-[1199px]:bg-white`}>
+        <header className={`fixed z-30 inset-x-0 min-[810px]:px-[20px] px-[15px] min-[810px]:h-[58px] h-[49px] grid items-center min-[1200px]:grid-cols-3 grid-cols-2 gap-[20px] font-semibold min-[810px]:text-[18px] text-[14px] tracking-[-0.03em] leading-none ${initBgOpaque ? "min-[1200px]:bg-white" : "min-[1200px]:text-white"} max-[1200px]:bg-white`}>
             <div className="">
                 <Link href="/" className="hover:opacity-40 transition-opacity">T.Q.C</Link>
             </div>
-            <nav className={`min-[1200px]:col-start-3 max-[809px]:grid ${navOpened ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} transition-all ease-in-out duration-500 max-[809px]:absolute top-full inset-x-0`}>
+            <nav className={`min-[1200px]:col-start-3 max-[810px]:grid ${navOpened ? "grid-rows-[1fr]" : "grid-rows-[0fr]"} transition-all ease-in-out duration-500 max-[810px]:absolute top-full inset-x-0`}>
                 <div className="overflow-hidden">
-                    <div className="max-[809px]:px-[15px] max-[809px]:pb-[100px] max-[809px]:flex flex-col justify-center max-[809px]:text-[60px] max-[809px]:h-[calc(100dvh_-_49px)] max-[809px]:bg-white">
+                    <div className="max-[810px]:px-[15px] max-[810px]:pb-[100px] max-[810px]:flex flex-col justify-center max-[810px]:text-[60px] tracking-[-0.03em] max-[810px]:h-[calc(100dvh_-_49px)] max-[810px]:bg-white">
                         {navLinks.map((link, linkIndex) => {
                             return (
                                 <span key={linkIndex}>
                                     <Link href={link.href} className="hover:opacity-40 transition-opacity">
                                         {link.text}
                                     </Link>
-                                    <span className="max-[809px]:hidden">
+                                    <span className="max-[810px]:hidden">
                                         {linkIndex < navLinks.length - 1 ? ", " : ""}
                                     </span>
                                 </span>
