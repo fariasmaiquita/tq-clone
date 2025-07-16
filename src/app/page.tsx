c855ae55626794b890eb9f3ac4e48635c6df8965
Farias Maiquita <fmaiquita@gmail.com>
2025-07-16 03:50:50 +0200

    Add AboutSection component to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import PageHeader from "@/app/components/PageHeader";
 import HeroSection from "@/app/components/HeroSection";
+import AboutSection from "@/app/components/AboutSection";
 
 export default function Home() {
     return (
@@ -8,6 +9,7 @@ export default function Home() {
             <PageHeader initBgOpaque={false} />
             <main className="">
                 <HeroSection />
+                <AboutSection />
             </main>
         </>
     );
