e7c6b6a2f97507433c04991817f330ddeaffa9f1
Farias Maiquita <fmaiquita@gmail.com>
2025-07-17 16:49:16 +0200

    Add NavData.ts with header + footer nav links data

---
+++ src/app/data/NavData.ts
@@ -0,0 +1,12 @@
+export const HeaderNavData = [
+    { href: "", text: "Work" },
+    { href: "", text: "Studio" },
+    { href: "", text: "News" },
+    { href: "", text: "Careers" },
+    { href: "", text: "Contact" }
+];
+
+export const FooterNavData = [
+    ...HeaderNavData,
+    { href: "", text: "Privacy" },
+]
