e335798c58cb1169e7d63d14fa837e14270942ff
Farias Maiquita <fmaiquita@gmail.com>
2025-07-17 07:00:24 +0200

    Add NewsData.ts with news content

---
+++ src/app/data/NewsData.ts
@@ -0,0 +1,50 @@
+export const NewsData = [
+    {
+        title: "How We Create Impactful Brand Identities Through Our Process",
+        category: "Behance",
+        date: "October 7, 2024",
+        imgSrc: "/img/news/tqc-news-1.avif"
+    }, {
+        title: "The Role of Color Theory in ",
+        category: "The Brand Identity",
+        date: "October 19, 2024",
+        imgSrc: "/img/news/tqc-news-2.avif"
+    }, {
+        title: "Announcing an Exciting Collaboration with Leading Motion Designers",
+        category: "Visuelle",
+        date: "November 2, 2024",
+        imgSrc: "/img/news/tqc-news-3.avif"
+    }, {
+        title: "Maximizing Brand Value Through Strategic Brand Visuals",
+        category: "Awwwards",
+        date: "November 22, 2024",
+        imgSrc: "/img/news/tqc-news-4.avif"
+    }, {
+        title: "Innovative Approaches and Thoughts to Branding in Digital Spaces",
+        category: "Studio Updates",
+        date: "December 06, 2024",
+        imgSrc: "/img/news/tqc-news-5.avif"
+    }, {
+        title: "New Creative Director Ethan Brooks Joins to Elevate Our Vision",
+        category: "Creative Review",
+        date: "December 18, 2024",
+        imgSrc: "/img/news/tqc-news-6.avif"
+    }, {
+        title: "Crafting Memorable Brand Identities with Timeless Designs",
+        category: "Site Inspire",
+        date: "January 04, 2025",
+        imgSrc: "/img/news/tqc-news-7.avif"
+    }, {
+        title: "Our Studio’s Recent Project Wins International Recognition",
+        category: "CSS Awards",
+        date: "January 30, 2025",
+        imgSrc: "/img/news/tqc-news-8.avif"
+    }, {
+        title: "Behind the Scenes of Our Award-Winning Brand Campaign",
+        category: "The Database",
+        date: "February 10, 2025",
+        imgSrc: "/img/news/tqc-news-9.avif"
+    }
+];
+
+export const LatestNewsData = NewsData.slice(0, 6);
