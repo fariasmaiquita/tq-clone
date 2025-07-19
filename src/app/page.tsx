f8a5e28c854acc150583609d5b181ad1d99105ef
Farias Maiquita <fmaiquita@gmail.com>
2025-07-19 23:58:53 +0200

    Replace previous animate classes with tailwindcss-motion utility classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import PageFooter from "@/app/components/PageFooter";
 
 export default function Home() {
     return (
-        <div className="animate-page-content">
+        <div className="motion-translate-y-in-[-100px] motion-opacity-in-0 motion-duration-700 motion-delay-300">
             <main>
                 <HeroSection />
                 <AboutSection />
