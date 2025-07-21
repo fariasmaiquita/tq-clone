37c8812cfe4b8f07b02a94c55d13b1a7565fa2be
Farias Maiquita <fmaiquita@gmail.com>
2025-07-21 19:25:44 +0200

    Add StudioIntro.tsx component

---
+++ src/app/components/StudioIntro.tsx
@@ -0,0 +1,16 @@
+export default function StudioIntro() {
+    return (
+        <section className="pt-[40px] min-[810px]:pb-[200px] pb-[150px] min-[810px]:px-[20px] px-[15px] grid min-[810px]:grid-cols-3 grid-cols-1 gap-[15px] leading-[1.3] font-medium">
+            <div className="min-[810px]:col-start-2">
+                <p>
+                    {"T.Q.C is a creative studio with a visionary approach to building timeless brands for the modern era. Based between London and New York, T.Q.C collaborates with forward-thinking companies to shape compelling, concept-driven visual identities through expertly crafted digital and printed mediums."}
+                </p>
+            </div>
+            <div className="min-[810px]:col-start-3">
+                <p>
+                    {"With a unique blend of creativity and precision, the studio transforms ideas into captivating stories that resonate with audiences. T.Q.C is more than just a studio—it’s a creative hub where innovation meets artistry, pushing the boundaries of visual communication."}
+                </p>
+            </div>
+        </section>
+    )
+}
