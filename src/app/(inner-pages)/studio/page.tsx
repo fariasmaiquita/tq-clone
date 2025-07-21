f06ad2ef7d248768fcdddee1a34d8700a0d77c4c
Farias Maiquita <fmaiquita@gmail.com>
2025-07-22 01:48:41 +0200

    Add Studio Team component to studio page.tsx

--- src/app/(inner-pages)/studio/page.tsx
+++ src/app/(inner-pages)/studio/page.tsx
@@ -1,11 +1,13 @@
 import StudioHero from "@/app/components/StudioHero";
 import StudioIntro from "@/app/components/StudioIntro";
+import StudioTeam from "@/app/components/StudioTeam";
 
 export default function StudioPage() {
     return (
         <main className="motion-translate-y-in-[-100px] motion-opacity-in-0 motion-duration-700 motion-delay-300">
             <StudioHero />
             <StudioIntro />
+            <StudioTeam />
         </main>
     )
 }
