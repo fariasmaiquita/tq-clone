41de707b29a8fd19fcfc3a3e2c23f475eb22086a
Farias Maiquita <fmaiquita@gmail.com>
2025-07-22 09:02:36 +0200

    Add studio client data

---
+++ src/app/data/ClientsData.ts
@@ -0,0 +1,42 @@
+export const ClientsData: {name: string, imgSrc: string}[] = [
+    {
+        name: "Nike",
+        imgSrc: "/img/clients/tqc-client-01.avif"
+    }, {
+        name: "Gucci",
+        imgSrc: "/img/clients/tqc-client-02.avif"
+    }, {
+        name: "Aesop",
+        imgSrc: "/img/clients/tqc-client-03.avif"
+    }, {
+        name: "Louis Vuitton",
+        imgSrc: "/img/clients/tqc-client-04.avif"
+    }, {
+        name: "Porsche",
+        imgSrc: "/img/clients/tqc-client-05.avif"
+    }, {
+        name: "Hermès",
+        imgSrc: "/img/clients/tqc-client-06.avif"
+    }, {
+        name: "Ray-Ban",
+        imgSrc: "/img/clients/tqc-client-07.avif"
+    }, {
+        name: "Chanel",
+        imgSrc: "/img/clients/tqc-client-08.avif"
+    }, {
+        name: "Cartier",
+        imgSrc: "/img/clients/tqc-client-09.avif"
+    }, {
+        name: "Dior",
+        imgSrc: "/img/clients/tqc-client-10.avif"
+    }, {
+        name: "Moët & Chandon",
+        imgSrc: "/img/clients/tqc-client-11.avif"
+    }, {
+        name: "La Mer",
+        imgSrc: "/img/clients/tqc-client-12.avif"
+    }, {
+        name: "Sennheiser",
+        imgSrc: "/img/clients/tqc-client-13.avif"
+    }
+]
