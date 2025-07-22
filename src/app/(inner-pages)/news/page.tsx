dbb47dcce6e0a2d1bbd03e94c92beaddb3f2de79
Farias Maiquita <fmaiquita@gmail.com>
2025-07-22 17:31:51 +0200

    Update some sizes on news page.tsx

--- src/app/(inner-pages)/news/page.tsx
+++ src/app/(inner-pages)/news/page.tsx
@@ -10,14 +10,14 @@ export default function NewsPage() {
                 <div className="pt-[200px] pb-[100px] min-[1200px]:text-[60px] min-[810px]:text-[48px] text-[38px] tracking-[-.03em] max-w-[550px] font-semibold">
                     The home of news, insights and updates
                 </div>
-                <div className="flex flex-wrap gap-x-[15px] min-[810px]:gap-y-[30px] gap-y-[10px]">
+                <div className="flex flex-wrap gap-x-[15px] min-[810px]:gap-y-[30px] gap-y-[40px]">
                     {NewsData.map((itemData, itemIndex) => {
                         if (!itemData) return null;
                         const itemHref = `/news/${itemIndex}`;
                         return (
                             <div key={itemIndex} className={`${itemIndex > 1 ? "min-[810px]:grow min-[810px]:w-[calc((100%-38.54%-30px)/2)] max-[810px]:basis-full" : "min-[810px]:shrink-0 min-[810px]:w-[38.54%] max-[810px]:basis-full"}`}>
                                 <Link href={itemHref} className="block bg-black/40 aspect-4/3 relative rounded-[10px] overflow-hidden">
-                                    <FadeInImage src={itemData.imgSrc} sizes="(min-width: 810px) 33vw, 83vw" alt="" />
+                                    <FadeInImage src={itemData.imgSrc} sizes="(min-width: 810px) 33vw, calc(100vw - 30px)" alt="" />
                                 </Link>
                                 <Link href={itemHref} className="text-[14px] tracking-[-.03em] inline-flex flex-wrap gap-x-[5px] mt-[12px] opacity-40">
                                     <span>{itemData.category}</span>
@@ -25,7 +25,7 @@ export default function NewsPage() {
                                     <span>{itemData.date}</span>
                                 </Link>
                                 <br />
-                                <Link href={itemHref} className="min-[810px]:max-w-[360px] max-w-[320px] inline-block mt-[10px] pb-[30px] min-[810px]:text-[22px] text-[18px] tracking-[-.03em] font-semibold transition-opacity duration-500 hover:opacity-40">
+                                <Link href={itemHref} className="min-[810px]:max-w-[360px] max-w-[320px] inline-block mt-[10px] min-[810px]:text-[22px] text-[18px] tracking-[-.03em] font-semibold transition-opacity duration-500 hover:opacity-40">
                                     {itemData.title}
                                 </Link>
                             </div>
